a1f1eb0d1b3f87153f4e647fdab62ea8ef11685b
rashedul.islam <rashedmbstucse07@gmail.com>
2022-10-25 22:59:49 +0600

    Added Hello World Class

---
+++ src/test/java/com/academy/learning/sqa/testngtest/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.academy.learning.sqa.testngtest;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("SQA learning Academy");
+    }
+}
